fbeca499ce2b2de511122e1f3efb45395d2e4072
ashwindaniel <d.redvirus@gmail.com>
2017-08-16 19:16:14 +0530

    16/aug/2017
    location

--- js/controller/mainmodule.js
+++ js/controller/mainmodule.js
@@ -10,10 +10,34 @@ angular.module('DemoModule', ['ngRoute'])
             .when('/forms', {
                 templateUrl: '../../../pages/forms.html'
             })
+            .when('/location', {
+                templateUrl: '../../../pages/location.html',
+                controller: 'LocationCtrl'
+            })
             .otherwise({
                 templateUrl: '../../../pages/start.html'
             })
     })
     .controller('DemoCtrl', function($scope) {
         $scope.message = "Target scope";
+    })
+    .controller('LocationCtrl', function($scope) {
+        $scope.latitude = 0;
+        $scope.longitude = 0;
+        $scope.dataloaded = false;
+        $scope.runLocation = function() {
+            if (navigator.geolocation) {
+                $scope.position = navigator.geolocation.getCurrentPosition(storeLocation);
+            } else {
+                alert("Your browser doesn't supports it!");
+            }
+        };
+
+        function storeLocation(position) {
+            $scope.latitude = position.coords.latitude;
+            $scope.longitude = position.coords.longitude;
+            $scope.dataloaded = true;
+        }
+
+
     });
