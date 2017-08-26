1afa63f2082c97393f67c59569bbfaedf4abe869
ashwindaniel <d.redvirus@gmail.com>
2017-08-27 03:07:21 +0530

    tested jshint and sass

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var gulputil = require('gulp-util');
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+
+gulp.task('sass', function () {
+    console.log("Converting scss to css");
+    var css = gulp.src('Source/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('Source/css'));
+    console.log("Conversion complete");
+    return css;
+});
+
+gulp.task('jshint', function () {
+
+    var js = gulp.src('Source/js/controller/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+    return js;
+});
+
+gulp.task('jscopy', function () {
+    gulp.src('Source/js/**/*.js')
+        .pipe(gulp.dest('Final/js'))
+});
+
+gulp.task('default', ['sass'], function () {
+    console.log("Starting build!");
+});
