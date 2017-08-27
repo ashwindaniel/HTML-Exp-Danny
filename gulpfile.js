de185b7d89e003753f54bb7195f3026cb7929a4b
ashwindaniel <d.redvirus@gmail.com>
2017-08-27 22:30:04 +0530

    some gulpfile.js coding only

--- gulpfile.js
+++ gulpfile.js
@@ -17,23 +17,17 @@ gulp.task('jshint', function () {
     var js = gulp.src('Source/js/controller/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
-    // .pipe(jshint.reporter('fail'));
-    console.log(js);
-    // if (js.jshint.success) {
-    //     gulp.src('Source/js/**/*.js')
-    //         .pipe(gulp.dest('Final/js'));
-    // }
-    // else {
-    //     console.log("Error in js files");
-    // }
     return js;
 });
 
 gulp.task('jscopy', function () {
     gulp.src('Source/js/**/*.js')
-        .pipe(gulp.dest('Final/js'))
+        .pipe(gulp.dest('Final/js'));
 });
 
-gulp.task('default', ['sass', 'jshint', 'jscopy'], function () {
-    console.log("Starting build!");
+gulp.task('watching', function () {
+    gulp.watch('Source/js/controller/**/*.js', ['jshint', 'jscopy']);
+});
+
+gulp.task('default', ['sass', 'watching'], function () {
 });
