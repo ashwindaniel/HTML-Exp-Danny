a23ab1e4f590394875714e4344fc85057b1a502a
ashwindaniel <d.redvirus@gmail.com>
2017-09-04 04:59:13 +0530

    basic commit

--- Final/js/controller/myapp-src.js
+++ Final/js/controller/myapp-src.js
@@ -1,6 +1,6 @@
 var x = 0;
 angular.module('DemoModule', ['ngRoute'])
-    .config(function($routeProvider) {
+    .config(function ($routeProvider) {
         $routeProvider
             .when('/first', {
                 templateUrl: "../../pages/first.html"
@@ -11,9 +11,12 @@ angular.module('DemoModule', ['ngRoute'])
             .when('/forms', {
                 templateUrl: '../../pages/forms.html'
             })
+            .when('/basic', {
+                templateUrl: '../../pages/BasicTraining.html'
+            })
             .when('/location', {
                 templateUrl: '../../pages/location.html'
-                    // controller: 'LocationCtrl'
+                // controller: 'LocationCtrl'
             })
             .when('/video', {
                 templateUrl: '../../pages/video.html'
@@ -43,27 +46,27 @@ angular.module('DemoModule', ['ngRoute'])
                 };
                 var map = new google.maps.Map(iElement[0], mapOptions);
 
-                scope.$watch('center', function() {
+                scope.$watch('center', function () {
                     map.setCenter(new google.maps.LatLng(
                         parseFloat(scope.center.lat),
                         parseFloat(scope.center.lng)
                     ));
                 }, true);
 
-                scope.$watch('zoom', function() {
+                scope.$watch('zoom', function () {
                     map.setZoom(parseInt(scope.zoom));
                 });
 
-                google.maps.event.addListener(map, 'center_changed', function() {
-                    $timeout(function() {
+                google.maps.event.addListener(map, 'center_changed', function () {
+                    $timeout(function () {
                         var center = map.getCenter();
                         scope.center.lat = center.lat();
                         scope.center.lng = center.lng();
                     });
                 });
 
-                google.maps.event.addListener(map, 'zoom_changed', function() {
-                    $timeout(function() {
+                google.maps.event.addListener(map, 'zoom_changed', function () {
+                    $timeout(function () {
                         scope.zoom = map.getZoom();
                     });
                 });
@@ -71,15 +74,15 @@ angular.module('DemoModule', ['ngRoute'])
             }
         };
     })
-    .controller('DemoCtrl', function($scope) {
+    .controller('DemoCtrl', function ($scope) {
         $scope.message = "Target scope";
     })
-    .controller('LocationCtrl', function($scope) {
+    .controller('LocationCtrl', function ($scope) {
         $scope.latitude = 0;
         $scope.longitude = 0;
         $scope.dataloaded = false;
         var map;
-        $scope.runLocation = function() {
+        $scope.runLocation = function () {
             if (navigator.geolocation) {
                 $scope.position = navigator.geolocation.getCurrentPosition(storeLocation);
                 console.log("inside map");
